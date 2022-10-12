af016a8b0cb0da5e2e296207ac1a350580bc5c0e
James Gasek <jamesgasek@dyn229221.cc.lehigh.edu>
2022-10-12 16:13:45 -0400

    get URL

--- pf.py
+++ pf.py
@@ -1,23 +1,29 @@
+import os
+import glob
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-import os
-import glob
-#time
-import time
+
 
 options = webdriver.ChromeOptions()
-#options.add_argument('headless')
 
+path = os.path.dirname(os.path.abspath(__file__))
+prefs = {}
+#config for downloads to be in current directory
+prefs["profile.default_content_settings.popups"]=0
+prefs["download.default_directory"]=path
+options.add_experimental_option("prefs", prefs)
 
 pfdriver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 maildriver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-
+#create temporary email
 maildriver.get("https://hottempmail.com/")
 
+#go to planet fitness form and fill out
 pfdriver.get('https://daypass.planetfitness.com/')
 
 select = Select(pfdriver.find_element('id','ContentPlaceHolder1_cmbCountry'))
@@ -29,8 +35,6 @@ select.select_by_visible_text('PA')
 select = Select(pfdriver.find_element('id','ContentPlaceHolder1_cmbCity'))
 select.select_by_visible_text('Bethlehem')
 
-print("Inserting name")
-time.sleep(10)
 
 fname = pfdriver.find_element('id','ContentPlaceHolder1_txtFirstName')
 fname.send_keys('John')
@@ -48,7 +52,7 @@ select = Select(pfdriver.find_element('id','ContentPlaceHolder1_ddlDOBYear'))
 select.select_by_visible_text('1990')
 
 address = maildriver.find_element('id','email_id')
-print(address.text)
+#print(address.text)
 
 email = pfdriver.find_element('id','ContentPlaceHolder1_txtEmailAddress')
 email.send_keys(address.text)
@@ -64,21 +68,35 @@ while True:
     except:
         time.sleep(1)
 
-print("Found email")
 #click on the email
 maildriver.find_element('xpath','//*[contains(text(),"Welcome to Planet Fitness")]').click()
 #click on download
-#maildriver.find_element('xpath','//*[contains(text(),"Download")]').click()
-#get the src of the image with width = 300
-time.sleep(1000)
-
-img = maildriver.find_element('xpath','//img[@width="300"]')
-print(img.get_attribute('src'))
-#get the most recent download from the downloads folder
+maildriver.find_element('xpath','//*[contains(text(),"Download")]').click()
 
 
-time.sleep(1000)
+#open the .eml file and find the full url of the link containing "bwipjs-api"
+#this is the link to the pass
+#wait until the file is downloaded
+while True:
+    try:
+        #open the .eml file in this directory
+        file = open(glob.glob("*.eml")[0], "r")
+        break
+    except:
+        time.sleep(1)
 
+#open the file
+with open(file.name, 'r') as f:
+    #read the file
+    data = f.read()
+    #fget line with link
+    line = data.splitlines()[413]
+    #extract url
+    url = line.split('src="')[1].split('"')[0]
+    print(url)
+
+#delete the file
+os.remove(file.name)
 
 pfdriver.quit()
 maildriver.quit()
